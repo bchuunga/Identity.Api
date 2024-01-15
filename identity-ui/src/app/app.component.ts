91d708fdfa45afef6821a18d452d38c8f131bb2b
Ben Chuunga <benjamin.chuunga@gmail.com>
2024-01-15 13:02:59 -0800

    Initial commit

---
+++ identity-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'identity-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'identity-ui';
+}
